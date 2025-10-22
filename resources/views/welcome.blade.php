da2a6e45c7a3cb77a2c38a577fd14d6aa5bc6023
EnderArcherBoy <enderarcherboy@gmail.com>
2025-10-22 12:52:33 +0700

    Revert "Refactor welcome view for improved readability and maintainability"
    This reverts commit d54b582d809948d2fa085b7930673b3a8746f9de.

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -52,8 +52,8 @@
         <div class="flex items-center justify-center w-full transition-opacity opacity-100 duration-750 lg:grow starting:opacity-0">
             <main class="flex max-w-[335px] w-full flex-col-reverse lg:max-w-4xl lg:flex-row">
                 <div class="text-[13px] leading-[20px] flex-1 p-6 pb-12 lg:p-20 bg-white dark:bg-[#161615] dark:text-[#EDEDEC] shadow-[inset_0px_0px_0px_1px_rgba(26,26,0,0.16)] dark:shadow-[inset_0px_0px_0px_1px_#fffaed2d] rounded-bl-lg rounded-br-lg lg:rounded-tl-lg lg:rounded-br-none">
-                    <h1 class="mb-1 font-medium">stop hadouken in tekken 7</h1>
-                    <p class="mb-2 text-[#706f6c] dark:text-[#A1A09A]">Laravel has an incredibly rich ecosystem. <br>We suggest starting with the following.</p>
+                    <h1 class="mb-1 font-medium">My Laravel App</h1>
+                    <p class="mb-2 text-[#706f6c] dark:text-[#A1A09A]">IDK</p>
                     <ul class="flex flex-col mb-4 lg:mb-6">
                         <li class="flex items-center gap-4 py-2 relative before:border-l before:border-[#e3e3e0] dark:before:border-[#3E3E3A] before:top-1/2 before:bottom-0 before:left-[0.4rem] before:absolute">
                             <span class="relative py-1 bg-white dark:bg-[#161615]">
@@ -62,9 +62,9 @@
                                 </span>
                             </span>
                             <span>
-                                Read the
-                                <a href="https://laravel.com/docs" target="_blank" class="inline-flex items-center space-x-1 font-medium underline underline-offset-4 text-[#f53003] dark:text-[#FF4433] ml-1">
-                                    <span>Documentation</span>
+                                Follow me on
+                                <a href="https://github.com/EnderArcherBoy" target="_blank" class="inline-flex items-center space-x-1 font-medium underline underline-offset-4 text-[#f53003] dark:text-[#FF4433] ml-1">
+                                    <span>Github</span>
                                     <svg
                                         width="10"
                                         height="11"
@@ -121,7 +121,7 @@
                 <div class="bg-[#fff2f2] dark:bg-[#1D0002] relative lg:-ml-px -mb-px lg:mb-0 rounded-t-lg lg:rounded-t-none lg:rounded-r-lg aspect-[335/376] lg:aspect-auto w-full lg:w-[438px] shrink-0 overflow-hidden">
                   
                     {{-- my --}}
-                    <img src="{{ asset('images/mas.jpeg') }}" alt="my......sql" class="absolute top-1/2 left-1/2 w-[162px] h-[48px] lg:w-[218px] lg:h-[65px] -translate-x-1/2 -translate-y-1/2 select-none pointer-events-none" />
+                    <img src="{{ asset('images/test.png') }}" alt="my......sql" class="absolute top-1/2 left-1/2 w-[162px] h-[48px] lg:w-[218px] lg:h-[65px] -translate-x-1/2 -translate-y-1/2 select-none pointer-events-none" />
                     
                     <div class="absolute inset-0 rounded-t-lg lg:rounded-t-none lg:rounded-r-lg shadow-[inset_0px_0px_0px_1px_rgba(26,26,0,0.16)] dark:shadow-[inset_0px_0px_0px_1px_#fffaed2d]"></div>
                 </div>
