51c56369b920c10309e3049f7f13496b2d7d6cad
Shiki-12 <uknowndonp@gmail.com>
2025-10-22 12:27:14 +0700

    pull please

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -89,8 +89,8 @@
                                 </span>
                             </span>
                             <span>
-                                Watch video tutorials at
-                                <a href="https://laracasts.com" target="_blank" class="inline-flex items-center space-x-1 font-medium underline underline-offset-4 text-[#f53003] dark:text-[#FF4433] ml-1">
+                                Ini dari pull request
+                                <a href="https://google.com" target="_blank" class="inline-flex items-center space-x-1 font-medium underline underline-offset-4 text-[#f53003] dark:text-[#FF4433] ml-1">
                                     <span>Laracasts</span>
                                     <svg
                                         width="10"
